06d0efa81b270ac65e353d4c012440e6f41a6f9b
wingchunkf <bergmcj@comcast.net>
2018-04-24 10:47:29 -0700

    js to remove divs when buttons are clicked

--- main.js
+++ main.js
@@ -1,11 +1,79 @@
-searchPerson.addEventListener("click", function () {
+//when button is pressed, all divs fade out
+var mainContent = document.querySelector('.main-content');
+var charStatButton = document.querySelector('.char-stats');
 
-    var searchPerson = document.getElementById('searchPerson');
-    var button = document.getElementById('search');
-    var searchTerm = $('#searchInput').val();
-    var query = '?search=' + searchTerm;
+charStatButton.addEventListener("click", function () {
+    // var mainContent = document.querySelector('.main-content');
+    mainContent.classList.add('fadeOut');
+    console.log('what is happening?');
+})
+
+var speciesStatButton = document.querySelector('.species-stats');
+
+speciesStatButton.addEventListener("click", function () {
+
+    mainContent.classList.add('fadeOut');
+    console.log('species stat button was clicked');
+})
+
+// var planetsStatButton = document.querySelector('.planet-stats');
+
+// planetsStatButton.addEventListener("click", function () {
+
+//     mainContent.classList.add('fadeOut');
+//     console.log('planets stat button was clicked');
+// })
+
+// var starshipStatButton = document.querySelector('.starship-stats');
+
+// starshipStatButton.addEventListener("click", function () {
+
+//     mainContent.classList.add('fadeOut');
+//     console.log('starship stat button was clicked');
+// })
+
+// var vehicleStatButton = document.querySelector('.vehicle-stats');
+
+// vehicleStatButton.addEventListener("click", function () {
+   
+//     mainContent.classList.add('fadeOut');
+//     console.log('vehicle stat button was clicked');
+// })
+
+// var planetsStatButton = document.querySelector('.planet-stats');
+
+// planetsStatButton.addEventListener("click", function () {
+   
+//     mainContent.classList.add('fadeOut');
+//     console.log('planets stat button was clicked');
+// })
+
+
+
+var charButton = document.querySelector('.searchCharacters');
+
+charButton.addEventListener("click", function () { 
+
+    var charSearchInput = $('.searchCharInput').val();
+    var query = '?search=' + charSearchInput;
+
     var eyeColor = document.querySelector('.eyes');
     var skinColor = document.querySelector('.skin');    
+    
+
+    
+    
+
+ 
+
+
+
+
+
+
+
+
+
     var peopleUrl = "https://swapi.co/api/people/";
     var planetUrl = "https://swapi.co/api/planets/";
     var filmsUrl = "https://swapi.co/api/films/";
@@ -36,14 +104,33 @@ document.addEventListener('DOMContentLoaded', function() {
 var firstQuestion = document.querySelector('.question1');
 var secondQuestion = document.querySelector('.question2');
 var thirdQuestion = document.querySelector('.question3');
+var firstAnswer = document.querySelector('.answer1');
+var secondAnswer = document.querySelector('.answer2');
+var thirdAnswer = document.querySelector('.answer3');
+var questions = document.querySelector('.question-section');
 
-    firstQuestion.classList.add('fadeOne');
-    secondQuestion.classList.add('fadeTwo');
-    thirdQuestion.classList.add('fadeThree');
+    firstQuestion.classList.add('fadeIn');
+    secondQuestion.classList.add('fadeIn');
+    thirdQuestion.classList.add('fadeIn');
+    firstAnswer.classList.add('fadeIn');
+    secondAnswer.classList.add('fadeIn');
+    thirdAnswer.classList.add('fadeIn');
     firstQuestion.style.transitionDelay = "0.75s";
     secondQuestion.style.transitionDelay = "1.50s";
-    thirdQuestion.style.transitionDelay = "2.25s";
+    thirdQuestion.style.transitionDelay = "2.25s";    
+
+    firstAnswer.style.transitionDelay = "2.75s";
+    secondAnswer.style.transitionDelay = "3.25s";
+    thirdAnswer.style.transitionDelay = "4s";   
+    questions.style.transitionDelay = "6s";
+    questions.classList.add('fadeOut');
 });
+
+
+
+
+
+
 
 
 
